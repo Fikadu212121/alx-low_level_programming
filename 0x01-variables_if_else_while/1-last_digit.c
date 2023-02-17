1ac8547cc0c157e4ec489aece5353d25ab2d131e
Fikadu Tesema <fike.bdu@gmail.com>
2023-02-17 05:33:17 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,6 @@
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 
 {
@@ -28,4 +27,5 @@ int main(void)
 	if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
+
 }
