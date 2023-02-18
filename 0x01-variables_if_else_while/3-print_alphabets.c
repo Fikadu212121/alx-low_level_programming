c407f3a0be324398553a816c92c0cc6135682c8b
Fikadu Tesema <fike.bdu@gmail.com>
2023-02-17 22:12:55 -0800

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,16 +13,16 @@ int main(void)
 
 	 c = 'a';
 	 d = 'A';
-	 while
+	 while 
 		 (c <= 'z') {
 			 putchar(c);
-			 c++;
+			 c++; 
 		 }
-	 while
+	 while 
 		 (d <= 'Z') {
 			 putchar(d);
-			 d++;
-		 }
-	putchar('\n');
-	return (0);
+			 d++; 
+		 } 
+	 putchar('\n'); 
+	 return (0);
 }
