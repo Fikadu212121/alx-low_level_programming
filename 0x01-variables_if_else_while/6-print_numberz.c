34e0f987c382de68284d1a537aeb673014690dd4
Fikadu Tesema <fike.bdu@gmail.com>
2023-02-18 00:52:58 -0800

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,15 +6,15 @@
  */
 
 int main(void)
-{ 
-	int c; 
+{
+	int c;
 	
-	c = 0; 
-	while 
-		(c < 10) { 
-			putchar(c + '0'); 
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
 			c++;
-		} 
+		}
 	putchar('\n'); 
 	return (0);
 }
