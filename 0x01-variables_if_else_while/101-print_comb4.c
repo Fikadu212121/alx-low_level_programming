5ee2a2f5f39747456791cfd49d73b4fe12d7985d
Fikadu Tesema <fike.bdu@gmail.com>
2023-02-19 00:36:18 -0800

    combination of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,27 +7,26 @@
 int main(void)
 {
         int a;
-        int b;
+	int b;
 	int c;
-
-        for (a = 0; a <= 7; a++)
-        {
-                for (b = a + 1; b < 9; b++)
+	
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = a + 1; b < 9; b++)
 		{
 			for (c = b + 1; c < 10; c++)
-                {
-                        putchar('0' + a);
-                        putchar('0' + b);
-			putchar('0' + c);
-                        if (a < 7)
-                        {
-                                putchar(',');
-                                putchar(' ');
+			{
+				putchar('0' + a);
+				putchar('0' + b);
+				putchar('0' + c);
+				if (a < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-                 }
-
-                }
-        }
+		}
+	}
         putchar('\n');
         return (0);
 }
